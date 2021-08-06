ef6180e6eb04bb725045a6931bdcdcf6583afce5
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-08-07 00:29:03 +0200

    Try to convert icons

--- mkico.sh
+++ mkico.sh
@@ -5,15 +5,19 @@ DIR="debian/$PACKAGE-client/usr/share/icons/hicolor"
 
 resolutions='16 32 48 64 128 255 512'
 
+inkscap -V
+if [ `inkscape -V | awk '{print $2}'| awk -F. '{print $1}'` -eq 0 ]; then
+    command="inkscape -z "
+else
+    command="inkscape"
+fi
+
 for resolution in $resolutions
 do
     echo ${resolution}x${resolution}
     mkdir -p $DIR/${resolution}x${resolution}/
-    if [ `inkscape -V | awk '{print $2}'| awk -F. '{print $1}'` -eq 0 ]; then
-        inkscape -z -w ${resolution} -h ${resolution} $SRC -e $DIR/${resolution}x${resolution}/$PACKAGE.png
-    else
-        inkscape -w ${resolution} -h ${resolution} $SRC -e $DIR/${resolution}x${resolution}/$PACKAGE.png
-    fi
+    echo ${command} -w ${resolution} -h ${resolution} $SRC -e $DIR/${resolution}x${resolution}/$PACKAGE.png
+    ${command} -w ${resolution} -h ${resolution} $SRC -e $DIR/${resolution}x${resolution}/$PACKAGE.png
 done
 mkdir -p $DIR/scalable
 cp $SRC $DIR/scalable/$PACKAGE.svg 
