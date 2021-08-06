afdb11c095a6f8d078de4287262f2c38ddb8d0fe
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-08-07 00:12:14 +0200

    Try to convert images

--- mkico.sh
+++ mkico.sh
@@ -7,9 +7,13 @@ resolutions='16 32 48 64 128 255 512'
 
 for resolution in $resolutions
 do
-    echo $resolution
+    echo ${resolution}x${resolution}
     mkdir -p $DIR/${resolution}x${resolution}/
-    inkscape -z -w ${resolution} -h ${resolution} $SRC -e $DIR/${resolution}x${resolution}/$PACKAGE.png
+    if [ `inkscape -V | awk '{print $2}'| awk -F. '{print $1}'` -eq 0 ]; then
+        inkscape -z -w ${resolution} -h ${resolution} $SRC -e $DIR/${resolution}x${resolution}/$PACKAGE.png
+    else
+        inkscape -w ${resolution} -h ${resolution} $SRC -e $DIR/${resolution}x${resolution}/$PACKAGE.png
+    fi
 done
 mkdir -p $DIR/scalable
 cp $SRC $DIR/scalable/$PACKAGE.svg 
